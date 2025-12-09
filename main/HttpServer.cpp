b91acf6d927f767401347b0894c7835868c2145e
ole-hc <hofmann.ole.eibach@gmail.com>
2025-12-09 17:34:34 +0100

    Add first uri handler

--- main/HttpServer.cpp
+++ main/HttpServer.cpp
@@ -3,9 +3,34 @@
 const char* HttpServer::TAG = "Http-server";
 
 HttpServer::HttpServer()
+    : httpConfig(HTTPD_DEFAULT_CONFIG()), httpServer(NULL)
 {
+    index_uri = {
+        .uri       = "/",               // the address at which the resource can be found
+        .method    = HTTP_GET,          // The HTTP method (HTTP_GET, HTTP_POST, ...)
+        .handler   = indexGetHandler,   // The function which process the request
+        .user_ctx  = NULL               // Additional user data for context
+    };
 }
 
 HttpServer::~HttpServer()
 {
+}
+
+void HttpServer::startHttpServer()
+{
+    ESP_ERROR_CHECK(httpd_start(&httpServer, &httpConfig));
+    esp_err_t httpd_register_uri_handler(httpd_handle_t server, const httpd_uri_t *index_uri);
+}
+
+void HttpServer::closeHttpServer()
+{
+    ESP_ERROR_CHECK(httpd_stop(&httpServer));
+}
+
+esp_err_t  HttpServer::indexGetHandler(httpd_req_t)
+{
+    const char* resp_str = "<h1>Hello World</h1>";
+    httpd_resp_send(req, resp_str, HTTPD_RESP_USE_STRLEN);
+    return ESP_OK;
 }
--- main/HttpServer.h
+++ main/HttpServer.h
@@ -1,12 +1,20 @@
 #include "esp_log.h"
 #include "esp_http_server.h"
 
+// great tutorial: https://developer.espressif.com/blog/2025/06/basic_http_server/
+
 class HttpServer
 {
 private:
     static const char* TAG;
+    httpd_config_t httpConfig;
+    httpd_handle_t httpServer;
+    static const httpd_uri_t index_uri;
 public:
     HttpServer();
     ~HttpServer();
+    void startHttpServer();
+    void closeHttpServer();
+    void indexGetHandler();
 };
 
